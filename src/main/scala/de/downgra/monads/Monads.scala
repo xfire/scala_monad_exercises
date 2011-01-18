ed80952d02e4ecb72cfde48879195832b68733a9
Rico Schiekel <fire@downgra.de>
2011-01-18 20:30:15 +0100

    add fmap implementation

--- src/main/scala/de/downgra/monads/Monads.scala
+++ src/main/scala/de/downgra/monads/Monads.scala
@@ -14,13 +14,11 @@ case class Identity[A](a: A)
  
 // Monad implementations
 object Monad {
-  // 2. Replace error("todo") with an implementation
   def ListMonad: Monad[List] = new Monad[List] {
     def flatMap[A, B](a: List[A], f: A => List[B]): List[B] = a.foldLeft(List[B]())((acc, v) => acc ++ f(v))
     def unital[A](a: A): List[A] = List(a)
   }
  
-  // 3. Replace error("todo") with an implementation
   def OptionMonad: Monad[Option] = new Monad[Option] {
     def flatMap[A, B](a: Option[A], f: A => Option[B]): Option[B] = a match {
       case Some(x) => f(x)
@@ -29,13 +27,11 @@ object Monad {
     def unital[A](a: A): Option[A] = Some(a)
   }
  
-  // 4. Replace error("todo") with an implementation
   def InterMonad: Monad[Inter] = new Monad[Inter] {
     def flatMap[A, B](a: Inter[A], f: A => Inter[B]): Inter[B] = Inter(i => f(a.f(i)).f(i))
     def unital[A](a: A): Inter[A] = Inter(_ => a)
   }
  
-  // 5. Replace error("todo") with an implementation
   def IdentityMonad: Monad[Identity] = new Monad[Identity] {
     def flatMap[A, B](a: Identity[A], f: A => Identity[B]): Identity[B] = f(a.a)
     def unital[A](a: A): Identity[A] = Identity(a)
@@ -43,36 +39,26 @@ object Monad {
 }
  
 object MonadicFunctions {
-  // 6. Replace error("todo") with an implementation
   def sequence[M[_], A](as: List[M[A]], m: Monad[M]): M[List[A]] = {
     as.foldRight(m.unital(List[A]()))((v, acc) => m.flatMap(v, (x: A) => m.flatMap(acc, (y: List[A]) => m.unital(x :: y))))
   }
  
-  // 7. Replace error("todo") with an implementation
-  def fmap[M[_], A, B](a: M[A], f: A => B, m: Monad[M]): M[B] =
-    error("todo")
+  def fmap[M[_], A, B](a: M[A], f: A => B, m: Monad[M]): M[B] = m.flatMap(a, ((x: A) => m.unital(f(x))))
  
   // 8. Replace error("todo") with an implementation
-  def flatten[M[_], A](a: M[M[A]], m: Monad[M]): M[A] =
-    error("todo")
+  def flatten[M[_], A](a: M[M[A]], m: Monad[M]): M[A] = error("todo")
  
   // 9. Replace error("todo") with an implementation
-  def apply[M[_], A, B](f: M[A => B], a: M[A], m: Monad[M]): M[B] =
-    error("todo")
+  def apply[M[_], A, B](f: M[A => B], a: M[A], m: Monad[M]): M[B] = error("todo")
  
   // 10. Replace error("todo") with an implementation
-  def filterM[M[_], A](f: A => M[Boolean], as: List[A]
-    , m: Monad[M]): M[List[A]] =
-    error("todo")
+  def filterM[M[_], A](f: A => M[Boolean], as: List[A], m: Monad[M]): M[List[A]] = error("todo")
  
   // 11. Replace error("todo") with an implementation
-  def replicateM[M[_], A](n: Int, a: M[A], m: Monad[M]): M[List[A]] =
-    error("todo: flatMap n times to produce a list")
+  def replicateM[M[_], A](n: Int, a: M[A], m: Monad[M]): M[List[A]] = error("todo: flatMap n times to produce a list")
  
   // 12. Replace error("todo") with an implementation
-  def lift2[M[_], A, B, C](f: (A, B) => C, a: M[A], b: M[B]
-    , m: Monad[M]): M[C] =
-    error("todo")
+  def lift2[M[_], A, B, C](f: (A, B) => C, a: M[A], b: M[B], m: Monad[M]): M[C] = error("todo")
  
   // lift3, lift4, etc. Interesting question: Can we have liftN?
 }
