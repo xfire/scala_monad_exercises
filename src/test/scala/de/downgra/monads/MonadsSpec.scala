2706deb691919c0bff425ec2aa951c5e73773608
Rico Schiekel <fire@downgra.de>
2011-01-18 20:30:06 +0100

    migrate tests into real unit tests

--- src/test/scala/de/downgra/monads/MonadsSpec.scala
+++ src/test/scala/de/downgra/monads/MonadsSpec.scala
@@ -5,7 +5,142 @@ import org.scalatest.matchers.ShouldMatchers
 
 class MonadSpec extends FunSuite with ShouldMatchers {
 
-  test("") {
+  import Monad._
+  import MonadicFunctions._
+
+  val plusOne = Inter(1+)
+  val multTwo = Inter(2*)
+  val squared = Inter(n => n*n)
+  val plus = (_: Int) + (_: Int)
+
+  // sequence --------------------------------
+
+  test("sequence on ListMonad") {
+    sequence(List(List(1, 2), List(3, 4)), ListMonad) should be (List(List(1, 3), List(1, 4), List(2, 3), List(2, 4)))
+  }
+
+  test("sequence on OptionMonad") {
+    sequence(List(Some(7), Some(8), Some(9)), OptionMonad) should be (Some(List(7, 8, 9)))
+    sequence(List(Some(7), None, Some(9)), OptionMonad) should be (None)
+  }
+
+  test("sequence on InterMonad") {
+    (sequence(List(plusOne, multTwo, squared), InterMonad) f 7) should be (List(8, 14, 49))
+  }
+
+  test("sequence on IdentityMonad") {
+    sequence(List(Identity(7), Identity(4)), IdentityMonad) should be (Identity(List(7, 4)))
+  }
+
+  // fmap --------------------------------
+
+  test("fmap on ListMonad") {
+    fmap(List(1, 2, 3), (x: Int) => x * 10, ListMonad) should be (List(10, 20, 30))
+  }
+
+  test("fmap on OptionMonad") {
+    fmap(Some(8), (x: Int) => x * 10, OptionMonad) should be (Some(80))
+    fmap(None: Option[Int], (x: Int) => x * 10, OptionMonad) should be (None)
+  }
+
+  test("fmap on InterMonad") {
+    (fmap(plusOne, (x: Int) => x * 10, InterMonad) f 7) should be (80)
+  }
+
+  test("fmap on IdentityMonad") {
+    fmap(Identity(9), (x: Int) => x * 10, IdentityMonad) should be (Identity(90))
+  }
+
+  // flatten --------------------------------
+
+  test("flatten on ListMonad") {
+   flatten(List(List(1, 2), List(3, 4)), ListMonad) should be (List(1, 2, 3, 4))
+  }
+
+  test("flatten on OptionMonad") {
+   flatten(Some(Some(8)), OptionMonad) should be (Some(8))
+   flatten(Some(None: Option[Int]), OptionMonad) should be (None)
+   flatten(None: Option[Option[Int]], OptionMonad) should be (None)
+  }
+
+  test("flatten on InterMonad") {
+   (flatten(Inter(a => Inter(a *)), InterMonad) f 7) should be (49)
+  }
+
+  test("flatten on IdentityMonad") {
+   flatten(Identity(Identity(8)), IdentityMonad) should be (Identity(8))
+  }
+
+  // apply --------------------------------
+
+  test("apply on ListMonad") {
+     apply(List((a: Int) => a + 1,
+                (a: Int) => a * 2,
+                (a: Int) => a % 2), List(1, 2, 3), ListMonad) should be (List(2, 3, 4, 2, 4, 6, 1, 0, 1))
+  }
+
+  test("apply on OptionMonad") {
+   apply(Some((a: Int) => a + 1), Some(8), OptionMonad) should be (Some(9))
+   apply(None: Option[Int => Int], Some(8), OptionMonad) should be (None)
+   apply(Some((a: Int) => a + 1), None: Option[Int], OptionMonad) should be (None)
+  }
+
+  test("apply on InterMonad") {
+   (apply(Inter(a => (b: Int) => a * b), Inter(1+), InterMonad) f 7) should be (56)
+  }
+
+  test("apply on IdentityMonad") {
+   apply(Identity((a: Int) => a + 1), Identity(7), IdentityMonad) should be (Identity(8))
+  }
+
+  // filterM --------------------------------
+
+  test("filterM on ListMonad") {
+   filterM((a: Int) => List(a > 2, a % 2 == 0), List(1, 2, 3), ListMonad) should be (
+     List(List(3), Nil, List(2, 3), List(2), List(3), Nil, List(2, 3), List(2)))
+  }
+
+  test("filterM on OptionMonad") {
+   filterM((a: Int) => Some(a > 1), List(1, 2, 3), OptionMonad) should be (Some(List(2, 3)))
+  }
+
+  test("filterM on InterMonad") {
+   (filterM((a: Int) => Inter(n => a * n % 2 == 0), List(1, 2, 3), InterMonad) f 7) should be (
+     List(2))
+  }
+
+  test("filterM on IdentityMonad") {
+   filterM((a: Int) => Identity(a > 1), List(1, 2, 3), IdentityMonad) should be (Identity(List(2, 3)))
+  }
+
+  // replicateM --------------------------------
+
+  test("replicateM on ListMonad") {
+   replicateM(2, List(7, 8), ListMonad) should be (List(List(7, 7), List(7, 8), List(8, 7), List(8, 8)))
+  }
+
+  test("replicateM on OptionMonad") {
+   replicateM(2, Some(7), OptionMonad) should be (Some(List(7, 7)))
+  }
+
+  test("replicateM on InterMonad") {
+   (replicateM(2, plusOne, InterMonad) f 7) should be (List(8, 8))
+  }
+
+  test("replicateM on IdentityMonad") {
+   replicateM(2, Identity(6), IdentityMonad) should be (Identity(List(6, 6)))
+  }
+
+  // lift2 --------------------------------
+
+  test("lift2 on ListMonad") {
+   lift2(plus, List(1, 2), List(3, 4), ListMonad) should be (List(4, 5, 5, 6))
+  }
+
+  test("lift2 on OptionMonad") {
+   lift2(plus, Some(7), Some(8), OptionMonad) should be (Some(15))
+   lift2(plus, Some(7), None: Option[Int], OptionMonad) should be (None)
+   lift2(plus, None: Option[Int], Some(8), OptionMonad) should be (None)
   }
 
 }
